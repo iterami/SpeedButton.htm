ecd2ca6eba320cfbdfadcd88596a63d1968117c8
Jan Horava <czechinthemail@gmail.com>
2013-07-13 22:06:01 -0700

    improved formatting

--- README.md
+++ README.md
@@ -1,1 +1,1 @@
-A JavaScript game in which you must click randomly arranged green or red buttons as quickly as possible. There are many settings with which you can customize your game experience, all of which are saved in localStorage.
+A web-based game in which you must click randomly arranged green or red buttons as quickly as possible. Users can customize their game experience and settings are saved in localStorage.
--- js/speedbutton.js
+++ js/speedbutton.js
@@ -1,16 +1,16 @@
 function decisecond(){
     if(get('game-mode').value === 1 && parseFloat(get('time').innerHTML) <= 0
        && get('max-time').value > 0){
-        /* if in max-time mode and time is less than or equal to 0 and max-time isn't 0 */
+        // if in max-time mode and time is less than or equal to 0 and max-time isn't 0
         stop();
 
     }else if(get('game-mode').value === 0 && get('max-points').value !== 0
              && parseInt(get('score').innerHTML) >= get('max-points').value){
-        /* if in max-points mode and score is not greater than max-points and max-points is not equal to 0 */
+        // if in max-points mode and score is not greater than max-points and max-points is not equal to 0
         stop();
 
     }else{
-        /* increase time if max-time is equal to 0 or in max-points mode, else decrease time */
+        // increase time if max-time is equal to 0 or in max-points mode, else decrease time
         get('time').innerHTML = ((parseFloat(get('time').innerHTML) + ((get('game-mode').value === 1
                                 && get('max-time').value > 0) ? -.1 : .1)).toFixed(1));
     }
@@ -28,17 +28,18 @@ function randomize_buttons(clicked_button_id){
     i = get(clicked_button_id).value;
 
     if(game_running){
-        /* stop game if clicking on a red button ends the game and clicked on a red button */
+        // stop game if clicking on a red button ends the game and clicked on a red button
         if(get('red-onclick').value === 1 && i.lastIndexOf('-', 0) === 0){
             stop();
             return;
         }
     }
 
-    /* increase or decrease time based on settings value for clicked button */
-    get('score').innerHTML = parseInt(get('score').innerHTML) + (i.lastIndexOf('+', 0) === 0 ? parseInt(get('green-points').value) : -parseInt(get('red-points').value));
+    // increase or decrease time based on settings value for clicked button
+    get('score').innerHTML = parseInt(get('score').innerHTML)
+      + (i.lastIndexOf('+', 0) === 0 ? parseInt(get('green-points').value) : -parseInt(get('red-points').value));
 
-    /* reset buttons to disabled, value=-, and black backgrounds if game has not ended with this click */
+    // reset buttons to disabled, value=-, and black backgrounds if game has not ended with this click
     i = grid_side * grid_side - 1;
     var p = (get('game-mode').value == 1 ||
              get('max-points').value == 0 ||
@@ -54,13 +55,15 @@ function randomize_buttons(clicked_button_id){
         get(i).disabled = 1;
     }while(i--);
 
-    /* if game has not ended with this click */
+    // if game has not ended with this click
     if(p){
         var ti = 0;
 
-        /* randomize locations of and setup green buttons that currently exist */
+        // randomize locations of and setup green buttons that currently exist
         if(get('green-frequency').value > 0){
-            i = get('green-frequency').value > (grid_side * grid_side) - 1 ? (grid_side * grid_side) - 1 : get('green-frequency').value - 1;
+            i = get('green-frequency').value > (grid_side * grid_side) - 1
+              ? (grid_side * grid_side) - 1
+              : get('green-frequency').value - 1;
             ti = i + 1;
             do{
                 do{
@@ -74,10 +77,12 @@ function randomize_buttons(clicked_button_id){
             }while(i--);
         }
 
-        /* if there are no green buttons or space for red buttons is available */
+        // if there are no green buttons or space for red buttons is available
         if(get('green-frequency').value == 0 || (get('grid-dimensions').value > 1 && (grid_side * grid_side) - ti > 0)){
-            /* create and randomize enough red buttons to fill available space or just number of red buttons */
-            i = get('red-frequency').value > (grid_side * grid_side) - ti - 1 ? (grid_side * grid_side) - ti - 1 : get('red-frequency').value - 1;
+            // create and randomize enough red buttons to fill available space or just number of red buttons
+            i = get('red-frequency').value > (grid_side * grid_side) - ti - 1
+              ? (grid_side * grid_side) - ti - 1
+              : get('red-frequency').value - 1;
             if(i >= 0){
                 do{
                     do{
@@ -116,7 +121,7 @@ function reset(){
 }
 
 function save(){
-    /* save settings into localStorage if they differ from default */
+    // save settings into localStorage if they differ from default
     i = 11;
     j = [
         'grid-dimensions',
@@ -145,7 +150,7 @@ function save(){
     j = 0;
 }
 function set_settings_disable(i){
-    /* enable or disable most setting inputs */
+    // enable or disable most setting inputs
     get('grid-dimensions').disabled = i;
     get('game-mode').disabled = i;
     get('green-frequency').disabled = i;
@@ -159,7 +164,7 @@ function set_settings_disable(i){
 }
 
 function start(){
-    /* verify settings are numbers and greater than or equal to 0 */
+    // verify settings are numbers and greater than or equal to 0
     i = 7;
     var j = [
         'max-points',
@@ -186,14 +191,14 @@ function start(){
         }
     }while(i--);
 
-    /* adjust margin-top of entire game */
+    // adjust margin-top of entire game
     get('lol-a-table').style.marginTop = get('y-margin').value + 'px';
 
-    /* save settings into localStorage and create game area */
+    // save settings into localStorage and create game area
     save();
     setup();
 
-    /* reset game buttons */
+    // reset game buttons
     i = grid_side * grid_side - 1;
     do{
         get(i).disabled = 1;
@@ -204,19 +209,19 @@ function start(){
         get(i).value = '-';
     }while(i--);
 
-    /* disable many setting inputs */
+    // disable many setting inputs
     set_settings_disable(1);
 
     get('score-max').innerHTML = '';
     get('time-max').innerHTML = '';
 
-    /* generate green and red buttons */
+    // generate green and red buttons
     randomize_buttons(random_number(grid_side * grid_side));
 
     get('score').innerHTML = 0;
     get('time').innerHTML = 0;
 
-    /* setup max-time or max-points displays */
+    // setup max-time or max-points displays
     if(get('game-mode').value === 1){
         get('time').innerHTML = get('max-time').value >= 0 ? get('max-time').value === '' ? 0 : get('max-time').value : 30;
         if(get('max-time').value > 0){
@@ -238,7 +243,7 @@ function start(){
 
 function setup(){
     if(get('start-button').disabled || grid_side != get('grid-dimensions').value){
-        /* fetch settings from localStorage */
+        // fetch settings from localStorage
         get('grid-dimensions').value = ls.getItem('speedbutton-0') === null ? 5 : parseInt(ls.getItem('speedbutton-0'));
         get('game-mode').value = ls.getItem('speedbutton-1') === null ? 1 : parseInt(ls.getItem('speedbutton-1'));
         get('max-points').value = ls.getItem('speedbutton-2') === null ? 50 : parseInt(ls.getItem('speedbutton-2'));
@@ -260,7 +265,7 @@ function setup(){
         get('lol-a-table').style.marginTop = get('y-margin').value + 'px';
         grid_side = get('grid-dimensions').value;
 
-        /* create game area buttons */
+        // create game area buttons
         var j = [''];
         for(i = 0; i < (grid_side * grid_side); i++){
             if(i % grid_side === 0 && i !== 0){
@@ -285,13 +290,13 @@ function stop(){
     clearInterval(interval);
     game_running = 0;
 
-    /* diable buttons to prevent further clicks */
+    // diable buttons to prevent further clicks
     i = grid_side * grid_side - 1;
     do{
         get(i).disabled = 1;
     }while(i--);
 
-    /* enable settings to allow editing */
+    // enable settings to allow editing
     set_settings_disable(0);
 
     get('start-button').value = 'Start (' + get('start-key').value + ')';
@@ -314,7 +319,7 @@ window.onkeydown = function(e){
         stop();
         start();
 
-    }else if(i === 27){/* ESC */
+    }else if(i === 27){// ESC
         stop();
     }
 };
