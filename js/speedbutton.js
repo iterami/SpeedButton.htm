40be5f124fa226e6af2da76ff15ffa4fd8b34633
honzi <czechinthemail@gmail.com>
2017-06-17 17:22:09 -0700

    moved audio creation into core_repo_init()

--- js/speedbutton.js
+++ js/speedbutton.js
@@ -126,6 +126,11 @@ function repo_escape(){
 
 function repo_init(){
     core_repo_init({
+      'audios': {
+        'boop': {
+          'duration': .1,
+        },
+      },
       'keybinds': {
         72: {
           'todo': function(){
@@ -147,12 +152,6 @@ function repo_init(){
       },
       'storage-menu': '<table><tr><td><input id=green-frequency><td>Green Frequency<tr><td><input id=green-points><td>Green Points<tr><td><select id=grid-dimensions><option value=1>1x1</option><option value=2>2x2</option><option value=3>3x3</option><option value=4>4x4</option><option value=5>5x5</option></select><td>Dimensions<tr><td><input id=max><td>Max <select id=game-mode><option value=0>Points</option><option value=1>Time</option></select><tr><td><input id=red-frequency><td>Red Frequency<tr><td><input id=red-points><td>Red Points<tr><td><select id=red-onclick><option value=0>Lose Points</option><option value=1>End Game</option></select><td>Red Click<tr><td><input id=y-margin><td>Y Margin</table>',
       'title': 'SpeedButton.htm',
-    });
-    core_audio_create({
-      'id': 'boop',
-      'properties': {
-        'duration': .1,
-      },
     });
 
     setup();
