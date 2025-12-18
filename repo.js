18a3b74bdd1648fa56c15f0083fcd921d83b7df6
Jan Hořava <czechinthemail@gmail.com>
2025-12-18 09:28:50 -0800

    included negative scores in confirmation checks

--- repo.js
+++ repo.js
@@ -129,7 +129,7 @@ function repo_init(){
     core_repo_init({
       'beforeunload': {
         'todo': function(event){
-            if(score > 0){
+            if(score !== 0){
                 core_escape(true);
                 event.preventDefault();
             }
