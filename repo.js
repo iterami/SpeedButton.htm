45eb3f23501cf15e84a0767a52d2db545c2c0bdf
Jan Hořava <czechinthemail@gmail.com>
2025-12-18 09:52:29 -0800

    changed start button ID to restart

--- repo.js
+++ repo.js
@@ -136,7 +136,7 @@ function repo_init(){
         },
       },
       'events': {
-        'start_button': {
+        'restart': {
           'onclick': start,
         },
       },
@@ -146,7 +146,7 @@ function repo_init(){
         'score': 0,
         'time': 0,
       },
-      'info': '<button id=start_button type=button>Restart</button>',
+      'info': '<button id=restart type=button>Restart</button>',
       'menu': true,
       'storage': {
         'grid_x': 5,
