39d2d60ef39865fa385578d5b6960e376430970f
Jan Hořava <czechinthemail@gmail.com>
2026-02-18 11:23:39 -0800

    mediumized start button

--- repo.js
+++ repo.js
@@ -146,7 +146,7 @@ function repo_init(){
         'score': 0,
         'time': 0,
       },
-      'info': '<button id=start type=button>Start New Game</button>',
+      'info': '<button class=medium id=start type=button>Start New Game</button>',
       'menu': true,
       'storage': {
         'grid_x': 5,
